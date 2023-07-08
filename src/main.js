2f5e3be0a6b7c0021720e9a9de5c55c3c997a20b
andiLeong <alwayssmile20150724@gmail.com>
2023-07-08 16:55:51 +0800

    set up @ alias

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import './index.css'
 import App from './App.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import Home from './components/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '@/components/Home.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 
 
