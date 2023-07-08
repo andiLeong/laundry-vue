e97a1c72224c521784cf9cdaef1a506689cbaec3
andiLeong <alwayssmile20150724@gmail.com>
2023-07-08 16:39:16 +0800

    init vite ,vue, vue-router, axios,tailwind css

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <h1 class="text-sky-600">Hello App!</h1>
+
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    
+  <p class="text-red-500 bg-blue-400">
+    <!-- use the router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+  <!-- route outlet -->
+  <!-- component matched by the route will render here -->
+  <router-view></router-view>
+
+
+
+</template>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import './index.css'
+import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: HelloWorld },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+})
+
+// 5. Create and mount the root instance.
+const app = createApp(App)
+.use(router)
+.mount('#app');
