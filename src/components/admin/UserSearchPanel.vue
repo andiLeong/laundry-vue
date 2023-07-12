c0ee3e814b180ba46ea64752e2f79099b5ebbb9d
andiLeong <alwayssmile20150724@gmail.com>
2023-07-12 15:11:50 +0800

    add user data table

---
+++ src/components/admin/UserSearchPanel.vue
@@ -0,0 +1,141 @@
+<template>
+    <form class="space-y-3 my-6" @submit.prevent="submit">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+            Search Panel
+        </h3>
+
+        <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+            <div class="sm:col-span-3">
+                <BaseInput
+                    labelClass="form-label"
+                    placeHolder="Search user phone number"
+                    class="mt-1 form-input"
+                    label="Phone"
+                    type="text"
+                    v-model="search.phone"
+                />
+            </div>
+
+            <div class="sm:col-span-3">
+                <BaseInput
+                    labelClass="form-label"
+                    placeHolder="Search customer first name"
+                    class="mt-1 form-input"
+                    label="First Name"
+                    type="text"
+                    v-model="search.first_name"
+                />
+            </div>
+
+            <div class="sm:col-span-2">
+                <BaseInput
+                    labelClass="form-label"
+                    placeHolder="Order Amount"
+                    class="mt-1 form-input"
+                    label="Amount Larger Than"
+                    type="text"
+                    v-model="search.order_amount_larger_than"
+                />
+            </div>
+
+            <div class="sm:col-span-2">
+                <BaseSelect
+                    labelClass="form-label"
+                    class="mt-1 form-select"
+                    :options="last_order_this_month"
+                    v-model="search.last_order_this_month"
+                    label="Last Order Within Current Month"
+                />
+            </div>
+
+            <div class="sm:col-span-2">
+                <BaseSelect
+                    labelClass="form-label"
+                    class="mt-1 form-select"
+                    :options="perPage"
+                    v-model="search.per_page"
+                    label="Display Per Page"
+                />
+            </div>
+        </div>
+
+        <!-- {{ search }} -->
+
+        <div class="pt-1">
+            <div class="flex justify-end">
+                <button
+                    type="button"
+                    @click.prevent="reset"
+                    class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                >
+                    Reset
+                </button>
+                <button
+                    type="submit"
+                    class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                >
+                    Search
+                </button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+import BaseInput from '@/components/forms/BaseInput.vue';
+import BaseSelect from '@/components/forms/BaseSelect.vue';
+
+export default {
+    components: {
+        BaseInput,
+        BaseSelect,
+    },
+
+    emits: ['search-query', 'reset-query'],
+    data() {
+        return {
+            search: {
+                per_page: 10,
+                last_order_this_month: null,
+                phone: null,
+                first_name: null,
+                order_amount_larger_than: null,
+            },
+            perPage: [10, 50, 100, 200],
+            last_order_this_month: [true, false],
+        };
+    },
+
+    computed: {},
+
+    methods: {
+        reset() {
+            this.search = {
+                per_page: 10,
+                last_order_this_month: null,
+                phone: null,
+                first_name: null,
+                order_amount_larger_than: null,
+            };
+
+            this.$emit('reset-query');
+        },
+
+        submit() {
+            let search = Object.fromEntries(
+                Object.entries(this.search).filter(([_, v]) => v != null),
+            );
+
+            if (search.per_page == 10) {
+                delete search.per_page;
+            }
+
+            if (Object.keys(search).length === 0) {
+                return;
+            }
+
+            this.$emit('search-query', search);
+        },
+    },
+};
+</script>
--- src/views/admin/User.vue
+++ src/views/admin/User.vue
@@ -1,16 +1,188 @@
 <template>
     <AdminLayout>
         <main class="flex-1 pb-8">
-            users
+            <section class="max-w-7xl mx-auto mt-10">
+                <!--                {{ users }}-->
+                <!-- {{ selected }} -->
+                <AppTableLayout>
+                    <template v-slot:title>
+                        <h2 class="text-gray-600">Users</h2>
+
+                        <div class="my-2">
+                            <button
+                                class="go-back-btn"
+                                @click.prevent="showPannel = !showPannel"
+                            >
+                                {{ showPannel ? 'hide panel' : 'open panel' }}
+                            </button>
+                        </div>
+
+                        <template v-if="showPannel">
+                            <UserSearchPanel
+                                @search-query="getherQuery"
+                                @reset-query="resetQuery"
+                            />
+                        </template>
+                    </template>
+
+                    <AppTable>
+                        <template v-slot:th>
+                            <th
+                                scope="col"
+                                class="table-heading"
+                                v-for="column in columns"
+                                :key="column"
+                            >
+                                <Sorting
+                                    :column="column"
+                                    :sortedColumn="defaultSortColumn"
+                                    @sort="orderQuery"
+                                    @sortedColumn="setDefaultSortColumn"
+                                />
+                            </th>
+                        </template>
+
+                        <template v-slot:tb>
+                            <tr
+                                :class="
+                                    index % 2 == 0 ? 'bg-white' : 'bg-gray-100'
+                                "
+                                v-for="(user, index) in users"
+                                :key="user.id"
+                            >
+                                <td class="table-data">
+                                    {{ user.id }}
+                                </td>
+
+                                <td class="table-data">
+                                    {{ user.phone }}
+                                </td>
+
+                                <td class="table-data">
+                                    {{ user.type }}
+                                </td>
+
+                                <td class="table-data">
+                                    {{ user.first_name }}
+                                </td>
+
+                                <td class="table-data">
+                                    {{ user.middle_name }}
+                                </td>
+
+                                <td class="table-data">
+                                    {{ user.last_name }}
+                                </td>
+
+                                <td class="table-data">
+                                    {{ user.last_order_date }}
+                                </td>
+
+                                <td class="table-data">
+                                    {{ user.total_order_amount }}
+                                </td>
+                            </tr>
+                        </template>
+                    </AppTable>
+                </AppTableLayout>
+
+                <div class="my-4">
+                    <Paginator
+                        :perSection="3"
+                        v-if="pagination.current_page"
+                        :pagination="pagination"
+                        @switched-page="switchPage"
+                    />
+                </div>
+            </section>
         </main>
     </AdminLayout>
 </template>
 
 <script setup>
-import AdminLayout from "@/components/admin/AdminLayout.vue";
+import AdminLayout from '@/components/admin/AdminLayout.vue';
+import Paginator from '@/components/Paginator.vue';
+import AppTable from '@/components/AppTable.vue';
+import AppTableLayout from '@/components/AppTableLayout.vue';
+import Sorting from '@/components/Sorting.vue';
+import _ from 'lodash';
+import { ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import UserSearchPanel from '@/components/admin/UserSearchPanel.vue';
 
+const route = useRoute();
+const router = useRouter();
+
+const endpoint = ref('/api/admin/user');
+const defaultSortColumn = ref('id');
+const columns = ref([
+    'id',
+    'phone',
+    'type',
+    'first name',
+    'middle name',
+    'last name',
+    'last order',
+    'total order amount',
+]);
+const per_page = ref(10);
+const users = ref([]);
+const pagination = ref({});
+const page = ref(route.query.page || 1);
+const queryString = ref({});
+const sortQuery = ref('order_by[]=id&direction[]=desc');
+const showPannel = ref(false);
+
+function fetch(page, query = '') {
+    return axios
+        .get(`${endpoint.value}?page=${page}&${query}`)
+        .then((response) => {
+            users.value = response.data.data;
+            pagination.value = response.data;
+            delete pagination.value.data;
+        });
+}
+
+function orderQuery(query) {
+    sortQuery.value = query;
+    fetch(page.value, toQueryString(queryString.value));
+}
+
+function getherQuery(query) {
+    Object.assign(queryString.value, query);
+    fetch(page.value, toQueryString(queryString.value));
+}
+
+function toQueryString(query) {
+    if (sortQuery.value === '') {
+        return new URLSearchParams(query).toString();
+    }
+    return new URLSearchParams(query).toString() + '&' + sortQuery.value;
+}
+
+function resetQuery() {
+    queryString.value = {};
+    sortQuery.value = '';
+    fetch(page.value);
+}
+
+function switchPage(page) {
+    router.replace({
+        name: 'admin-user',
+        query: {
+            page,
+        },
+    });
+}
+
+function setDefaultSortColumn(column) {
+    defaultSortColumn.value = column;
+}
+
+watch(
+    () => route.query.page,
+    (page) => fetch(page, toQueryString(queryString.value)),
+);
+
+fetch(page.value, toQueryString(queryString.value));
 </script>
-
-<style scoped>
-
-</style>
