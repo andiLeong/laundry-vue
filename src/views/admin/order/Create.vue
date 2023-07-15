bd303cbe8309debc7a91d6bc51594d0d99f7627c
andiLeong <alwayssmile20150724@gmail.com>
2023-07-15 21:46:27 +0800

    add a create order component inside the tab

--- src/views/admin/order/Create.vue
+++ src/views/admin/order/Create.vue
@@ -2,49 +2,7 @@
 import AdminLayout from '@/components/admin/AdminLayout.vue';
 import AppTab from '@/components/AppTab.vue';
 import AppTabs from '@/components/AppTabs.vue';
-import {ref} from 'vue';
-import BaseInput from '@/components/forms/BaseInput.vue';
-import {useRouter} from 'vue-router';
-import ErrorManager from '@/components/validation/ErrorManager.vue';
-import Errors from "@/model/Errors.js";
-import SubmitButton from "@/components/forms/SubmitButton.vue";
-import SearchUserPhone from "@/components/admin/SearchUserPhone.vue";
-import useFetchServices from "@/composable/useFetchServices.js";
-
-const {services} = useFetchServices();
-const service_id = ref(null);
-const user_id = ref(null);
-const amount = ref(null);
-const router = useRouter();
-
-const errors = ref({});
-const isLoading = ref(false);
-
-function serviceChanged(e) {
-    let serviceId = e.target.value;
-    service_id.value = serviceId;
-    amount.value = services.value.filter((service) => {
-        return service.id === parseInt(serviceId);
-    })[0].price;
-}
-
-function submit() {
-    isLoading.value = true;
-    axios
-        .post('api/admin/order', {
-            service_id: service_id.value,
-            amount: amount.value,
-            user_id: user_id.value,
-        })
-        .then(() => router.push({name: 'admin-order'}))
-        .catch((error) => {
-            let err = new Errors(error);
-            errors.value = err.handle();
-            console.log(errors.value)
-            isLoading.value = false;
-        });
-}
-
+import CreateOrder from "@/components/admin/CreateOrder.vue";
 </script>
 
 <template>
@@ -53,61 +11,7 @@ function submit() {
             <section class="max-w-6xl mx-auto mt-10">
                 <AppTabs id="create-order-tab">
                     <AppTab title="Without Promotion" :active="true">
-                        <form class="space-y-3 my-6" @submit.prevent="submit">
-                            <SearchUserPhone v-model="user_id"/>
-
-                            <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                                <div class="sm:col-span-2">
-                                    <label class="form-label">Service</label>
-                                    <select
-                                        class="field form-select mt-1"
-                                        :value="service_id"
-                                        @change="serviceChanged"
-                                    >
-                                        <option disabled value>
-                                            please select
-                                        </option>
-                                        <option
-                                            v-for="service in services"
-                                            :value="service.id"
-                                            :key="service.id"
-                                            :selected="
-                                                service.id === service_id
-                                            "
-                                        >
-                                            {{ service.name }}
-                                        </option>
-                                    </select>
-                                </div>
-
-                                <div class="sm:col-span-2">
-                                    <BaseInput
-                                        labelClass="form-label"
-                                        placeHolder="Amount"
-                                        class="mt-1 form-input"
-                                        label="Order Amount"
-                                        type="text"
-                                        v-model="amount"
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="pt-1">
-                                <div class="mb-2">
-                                    <ErrorManager
-                                        v-if="errors"
-                                        :errors="errors"
-                                    />
-                                </div>
-                                <div class="flex justify-start">
-                                    <SubmitButton
-                                        :loading="isLoading"
-                                        saving="Creating"
-                                        not-saving="Create"
-                                    />
-                                </div>
-                            </div>
-                        </form>
+                        <CreateOrder/>
                     </AppTab>
                     <AppTab title="With Promotion">
                         <p>Hello world again. second</p>
