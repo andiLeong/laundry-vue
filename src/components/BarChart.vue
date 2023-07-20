f5ecf7552d737f9cd3d2800bd951fb9de4899745
andiLeong <alwayssmile20150724@gmail.com>
2023-07-20 22:34:46 +0800

    add a bar chart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,33 @@
+<template>
+    <Bar
+        id="my-chart-id"
+        :options="option"
+        :data="data"
+    />
+</template>
+
+<script setup>
+import {ref} from "vue";
+import {Bar} from 'vue-chartjs'
+import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js'
+
+defineProps(['data'])
+const option = ref({
+    responsive: true,
+    animations: {
+        tension: {
+            duration: 1000,
+            easing: 'linear',
+            from: 1,
+            to: 0,
+            loop: true
+        }
+    },
+})
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+</script>
+
+<style scoped>
+
+</style>
--- src/views/admin/Stat.vue
+++ src/views/admin/Stat.vue
@@ -6,7 +6,14 @@
                     <h2 class="text-lg font-medium leading-6 text-gray-900">
                         Overview
                     </h2>
-                    <StatCollection />
+                    <StatCollection/>
+
+
+                    <div class="mt-10 max-w-3xl mx-auto">
+                        <BarChart :data="chartData"/>
+                    </div>
+
+
                 </div>
             </div>
         </main>
@@ -16,6 +23,47 @@
 <script setup>
 import AdminLayout from '@/components/admin/AdminLayout.vue';
 import StatCollection from '@/components/admin/StatCollection.vue';
+import {ref} from "vue";
+import BarChart from "@/components/BarChart.vue";
+import Errors from "@/model/Errors.js";
+
+
+const chartData = ref({
+    labels: ['January', 'February', 'March'],
+    datasets: [
+        {
+            label: "Blue",
+            fillColor: "blue",
+            data: [3, 7, 4],
+            borderColor: '#36A2EB',
+            backgroundColor: '#9BD0F5',
+        },
+        {
+            label: "Green",
+            fillColor: "green",
+            data: [7, 2, 6],
+            borderColor: '#36A2EB',
+            backgroundColor: '#FFB1C1',
+        }
+    ]
+})
+
+function fetch() {
+    axios
+        .get('/api/admin/order-stats')
+        .then(({data}) => {
+
+            console.log(data);
+        })
+        .catch((error) => {
+            let err = new Errors(error);
+            errors.value = err.handle();
+        })
+        .finally(() => {
+        });
+}
+
+fetch();
 </script>
 
 <style scoped></style>
