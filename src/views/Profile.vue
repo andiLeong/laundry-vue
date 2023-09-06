901df4f7d38b47814da949dbc19c6953d0641caa
andiLeong <alwayssmile20150724@gmail.com>
2023-09-06 17:17:20 +0800

    add svg pattern to user profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -46,7 +46,7 @@ const { handleSubmit, errors } = useForm({
 });
 
 // const { value: phone } = useField('phone');
-const { value: first_name } = useField('first_name', 'andi');
+const { value: first_name } = useField('first_name');
 const { value: middle_name } = useField('middle_name');
 const { value: last_name } = useField('last_name');
 // const { value: password } = useField('password');
@@ -59,7 +59,6 @@ watch(updated, (newValue) => {
     if (newValue === true) {
         setTimeout(() => {
             updated.value = false;
-            console.log('hide it');
         }, 1000);
     }
 });
@@ -105,7 +104,7 @@ function update(attributes) {
                 <div class="col-span-4">
                     <main class="px-4 sm:px-6 lg:flex-auto lg:px-0">
                         <div
-                            class="md:grid grid-cols-3 space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none"
+                            class="md:grid grid-cols-3 lg:mx-0 lg:max-w-none gap-3"
                         >
                             <div class="col-span-2">
                                 <h2
@@ -220,6 +219,20 @@ function update(attributes) {
                                     </div>
                                 </form>
                             </div>
+
+                            <div class="col-span-1 relative overflow-hidden">
+                                <div
+                                    style="
+                                        height: 280px;
+                                        width: 280px;
+                                        right: -70px;
+                                        top: -70px;
+                                        clip-path: circle(50% at 53% 49%);
+                                    "
+                                    class="absolute"
+                                    id="svg"
+                                ></div>
+                            </div>
                         </div>
                     </main>
                 </div>
@@ -229,4 +242,9 @@ function update(attributes) {
     </MainLayout>
 </template>
 
-<style scoped></style>
+<style scoped>
+#svg {
+    background-color: #ffffff;
+    background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%2394a3b8' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
+}
+</style>
