c577d657a43a2c47be95024d23188e8b51fb89e8
andiLeong <alwayssmile20150724@gmail.com>
2023-10-03 11:09:16 +0800

    changed admin default avatar to log

--- src/components/admin/AdminHeader.vue
+++ src/components/admin/AdminHeader.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { useUserStore } from '@/store/user';
-
-const userStore = useUserStore();
 import { ref } from 'vue';
 import CheckCircle from '@/svg/CheckCircle.vue';
 import Building from '@/svg/Building.vue';
+
+const userStore = useUserStore();
 
 const firstName = ref(userStore.firstName);
 </script>
@@ -20,15 +20,15 @@ const firstName = ref(userStore.firstName);
                     <div class="flex items-center">
                         <img
                             class="hidden h-16 w-16 rounded-full sm:block"
-                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80"
-                            alt=""
+                            src="/default-avatar.png"
+                            alt="profile-logo-pic"
                         />
                         <div>
                             <div class="flex items-center">
                                 <img
                                     class="h-16 w-16 rounded-full sm:hidden"
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.6&w=256&h=256&q=80"
-                                    alt=""
+                                    src="/default-avatar.png"
+                                    alt="profile-logo-pic"
                                 />
                                 <h1
                                     class="ml-3 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:leading-9"
--- src/components/admin/AdminTopNavigation.vue
+++ src/components/admin/AdminTopNavigation.vue
@@ -56,8 +56,8 @@
                             >
                                 <img
                                     class="h-8 w-8 rounded-full"
-                                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt=""
+                                    src="/default-avatar.png"
+                                    alt="profile-logo-pic"
                                 />
                                 <span
                                     class="ml-3 hidden text-sm font-medium text-gray-700 lg:block"
