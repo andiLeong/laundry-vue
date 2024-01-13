700567a4104da8d39a90ff9d0ca0b4469502e9de
andiLeong <alwayssmile20150724@gmail.com>
2024-01-13 15:21:29 +0800

    add a test file for label not show for ios 2

--- src/components/admin/CreateOrderLabel.vue
+++ src/components/admin/CreateOrderLabel.vue
@@ -1,41 +1,53 @@
 <template>
-    <label>label outside form</label>
+    <AdminLayout>
+        <main class="flex-1 pb-8">
+            <section class="max-w-6xl lg:mx-auto mt-10 mx-4">
+                <label>label outside form</label>
 
-    <form>
-        <div class="space-y-3 my-6">
-            <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                <div class="sm:col-span-2">
-                    <label class="form-label"
-                        >With form label class without for attribute</label
-                    >
-                    <select class="field form-select mt-1">
-                        <option value="1">Cash</option>
-                        <option value="2">Gcash</option>
-                    </select>
-                </div>
+                <form>
+                    <div class="space-y-3 my-6">
+                        <div
+                            class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6"
+                        >
+                            <div class="sm:col-span-2">
+                                <label class="form-label"
+                                    >With form label class without for
+                                    attribute</label
+                                >
+                                <select class="field form-select mt-1">
+                                    <option value="1">Cash</option>
+                                    <option value="2">Gcash</option>
+                                </select>
+                            </div>
 
-                <div class="sm:col-span-2">
-                    <label class=""> original label no class no for </label>
-                </div>
+                            <div class="sm:col-span-2">
+                                <label class="">
+                                    original label no class no for
+                                </label>
+                            </div>
 
-                <div class="sm:col-span-2">
-                    <label class="" for="dummy">
-                        original label no class has fot
-                    </label>
-                    <input type="text" id="dummy" />
-                </div>
+                            <div class="sm:col-span-2">
+                                <label class="" for="dummy">
+                                    original label no class has fot
+                                </label>
+                                <input type="text" id="dummy" />
+                            </div>
 
-                <div class="sm:col-span-2">
-                    <p>paragraph tag</p>
-                    <input type="text" id="paragraph" />
-                </div>
-            </div>
-        </div>
-    </form>
+                            <div class="sm:col-span-2">
+                                <p>paragraph tag</p>
+                                <input type="text" id="paragraph" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </main>
+    </AdminLayout>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
+import AdminLayout from '@/components/admin/AdminLayout.vue';
 
 const props = defineProps({
     services: {
