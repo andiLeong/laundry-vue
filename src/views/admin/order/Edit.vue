05315fbc0067089fbf15bd6e5a2927abe2248bc7
andiLeong <alwayssmile20150724@gmail.com>
2024-02-10 18:21:21 +0800

    use file pond to upload

--- src/views/admin/order/Edit.vue
+++ src/views/admin/order/Edit.vue
@@ -7,6 +7,9 @@ import Errors from '@/model/Errors.js';
 import useFetchServices from '@/composable/useFetchServices.js';
 import BaseInput from '@/components/forms/BaseInput.vue';
 import ErrorManager from '@/components/validation/ErrorManager.vue';
+import vueFilePond from 'vue-filepond';
+import 'filepond/dist/filepond.min.css';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.esm.js';
 
 const route = useRoute();
 const router = useRouter();
@@ -14,10 +17,12 @@ const id = ref(route.params.id);
 const endpoint = ref(`/api/admin/order/${id.value}`);
 const isLoading = ref(false);
 const errors = ref({});
-const images = ref([]);
+const pond = ref(null);
 
 const { services } = useFetchServices();
 const { loading, order, error } = useFetchOrder(endpoint.value);
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
 
 function serviceChanged(e) {
     let serviceId = e.target.value;
@@ -27,21 +32,14 @@ function serviceChanged(e) {
     )[0].price;
 }
 
-function selectFile(e) {
-    console.log(e.target.files);
-    for (let i = 0; i < e.target.files.length; i++) {
-        images.value.push(e.target.files[i]);
-    }
-}
-
 function update() {
     const payload = new FormData();
     payload.append('service_id', order.value.service_id);
     payload.append('amount', order.value.amount);
     payload.append('description', order.value.description);
     payload.append('_method', 'patch');
-    images.value.forEach(file => {
-        payload.append('image[]', file);
+    pond.value.getFiles().forEach(image => {
+        payload.append('image[]', image.file);
     });
     axios
         .post(endpoint.value, payload, {
@@ -134,55 +132,12 @@ function update() {
                                 </div>
 
                                 <div class="col-span-full">
-                                    <label
-                                        for="cover-photo"
-                                        class="block text-sm font-medium leading-6 text-gray-900"
-                                        >Order Photo</label
-                                    >
-                                    <div
-                                        class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
-                                    >
-                                        <div class="text-center">
-                                            <svg
-                                                class="mx-auto h-12 w-12 text-gray-300"
-                                                viewBox="0 0 24 24"
-                                                fill="currentColor"
-                                                aria-hidden="true"
-                                            >
-                                                <path
-                                                    fill-rule="evenodd"
-                                                    d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                                                    clip-rule="evenodd"
-                                                />
-                                            </svg>
-                                            <div
-                                                class="mt-4 flex text-sm leading-6 text-gray-600"
-                                            >
-                                                <label
-                                                    for="file-upload"
-                                                    class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                                                >
-                                                    <span>Upload a file</span>
-                                                    <input
-                                                        id="file-upload"
-                                                        name="file-upload"
-                                                        type="file"
-                                                        class="sr-only"
-                                                        @change="selectFile"
-                                                        multiple
-                                                    />
-                                                </label>
-                                                <p class="pl-1">
-                                                    or drag and drop
-                                                </p>
-                                            </div>
-                                            <p
-                                                class="text-xs leading-5 text-gray-600"
-                                            >
-                                                PNG, JPG, GIF up to 2MB
-                                            </p>
-                                        </div>
-                                    </div>
+                                    <file-pond
+                                        name="test"
+                                        ref="pond"
+                                        label-idle="Drop images here..."
+                                        v-bind:allow-multiple="true"
+                                    />
                                 </div>
                             </div>
                         </div>
