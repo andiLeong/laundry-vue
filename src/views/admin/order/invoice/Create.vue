eee3a63460c58a85a8c634b3f4052f9c32fd5fc0
andiLeong <alwayssmile20150724@gmail.com>
2024-03-20 17:19:23 +0800

    add a create invocie button on invoice page

--- src/views/admin/order/invoice/Create.vue
+++ src/views/admin/order/invoice/Create.vue
@@ -39,10 +39,23 @@ function create() {
             loading.value = false;
         });
 }
+
+function goToCreateInvoice() {
+    router.push({ name: 'admin-order-invoice-create' });
+}
 </script>
 
 <template>
     <AdminLayout>
+        <template v-slot:right-button>
+            <button
+                type="button"
+                @click.prevent="goToCreateInvoice"
+                class="inline-flex items-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+            >
+                Create Invoice
+            </button>
+        </template>
         <main class="flex-1 pb-8">
             <section class="max-w-lg mx-auto mt-40 flex justify-center">
                 <form @submit.prevent="create" class="w-full p-10 space-y-5">
